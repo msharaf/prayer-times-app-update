aa7d9dfee2d18f964a9a014a06ac273a7a19e77e
Mouhab Sharaf <Mouhab@mouhabs-mbp.lan>
2023-05-01 23:15:34 -0400

    Used new method for discord

--- send_discord.py
+++ send_discord.py
@@ -3,7 +3,7 @@ import os
 
 from discordwebhook import Discord
 
-discord_url=  os.environ['discord_url']
+discord_url=  os.environ['DISCORD_URL']
 discord = Discord(url=discord_url)
 discord.post(content=sys.argv[1])
 
