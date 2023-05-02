3f67408b6ff4fe1c34be1d554778182a12e09405
Mouhab Sharaf <Mouhab@mouhabs-mbp.lan>
2023-05-01 22:54:04 -0400

    Updated code

--- send_discord.py
+++ send_discord.py
@@ -2,8 +2,8 @@ import discord
 import sys
 import os
 
-# channel_number = os.environ['channel_number']
-# discord_client_secret = os.environ['discord_client_secret']
+channel_number = os.environ['CHANNEL_NUMBER']
+discord_client_secret = os.environ['DISCORD_CLIENT_SECRET']
 print(channel_number)
 print(discord_client_secret)
 # Set up a Discord client
@@ -20,11 +20,11 @@ async def on_ready():
     print(f'Logged in as {client.user}')
 
     # Send a message to the channel
-    channel_id = '1102667321081860148' # Replace with the ID of your channel
+    channel_id = channel_number # Replace with the ID of your channel
     message = sys.argv[1]
     await send_discord_message(channel_id, message)
     await client.close()
 
 # Start the client
-client.run('MTEwMjY2ODkzODQ4MDk3NTk0Mg.GxIRwO.Xm2ZpSt7PoGscvHWWyQCo6JJa4sPurfM10L3eI')
+client.run(discord_client_secret)
 
